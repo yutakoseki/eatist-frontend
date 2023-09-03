48d93de1dfbc05d6d725815dc0d083b25af17ab6
yuta koseki <entryofyuta@gmail.com>
2023-09-04 02:46:39 +0900

    バックエンドapi修正

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:5055/api",
+    // baseURL: "http://localhost:5055/api",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         "Content-Type": "application/json",
     }
