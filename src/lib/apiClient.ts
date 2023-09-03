66d60cbcd5b8e6b2dedcf3f561b5886e40c3684c
yuta koseki <entryofyuta@gmail.com>
2023-09-04 03:46:14 +0900

    apiClient修正

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     // baseURL: "http://localhost:5055/api",
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
     headers: {
         "Content-Type": "application/json",
     }
